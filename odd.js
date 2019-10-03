f3cf4ad698edf8cc36fde9a2e1d7d06b0d2e8109
Mikel Gascon <mikelgascon@outlook.com>
2019-10-03 15:22:56 +0000

    function check if num is odd

---
+++ odd.js
@@ -0,0 +1,11 @@
+const isOdd = function (num) {
+  return !(num % 2 === 0);
+
+}
+
+const tenisOdd = isOdd(10);
+const elevenisOdd = isOdd(11);
+
+console.log(tenisOdd);
+console.log(elevenisOdd);
+  
