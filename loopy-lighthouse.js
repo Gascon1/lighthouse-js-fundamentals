e6cb213cf773841b14ce64df1967e0fb4b7dcb92
Mikel Gascon <mikelgascon@outlook.com>
2019-10-03 18:02:29 +0000

    loopy-lighthouse was untracked

---
+++ loopy-lighthouse.js
@@ -0,0 +1,22 @@
+// for (let i = 100; i <= 200; i++) {
+//   if ( i % 3 === 0 && i % 4 === 0) {
+//     console.log("LoopyLighthouse");
+//   } else if ( i % 3 === 0) {
+//     console.log("Loopy");
+//   } else if (i % 4 === 0) {
+//     console.log("Lighthouse");
+//   } else {
+//     console.log(i);
+//   }    
+// }
+
+// function laugh(num) {
+//   let laughter = "ha";
+//   let repeat = num;
+//   let laughcounter ='';
+//     for (let i = 0; i != repeat; i++) {
+//       laughcounter += laughter;
+//     }
+//     return laughcounter;
+// }
+// console.log(laugh(2) + "!");  
